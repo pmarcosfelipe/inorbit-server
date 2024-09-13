d30d074f377954b0befb443d8d21396b431f57ef
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-09-13 22:22:06 +0100

    create first route createGoal

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,36 @@
 import fastify from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { createGoal } from '../functions/create-goals';
+import z from 'zod';
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desireWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desireWeeklyFrequency } = request.body;
+    await createGoal({
+      title,
+      desireWeeklyFrequency,
+    });
+  }
+);
 
 app
   .listen({
